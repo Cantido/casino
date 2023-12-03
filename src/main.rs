63ba9c1124df8ef8072ff4e4f6ab243073abab45
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-12-03 14:26:10 -0700

    Use TOML for state file

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ use std::{fmt, fs, fs::OpenOptions, io, io::prelude::*, io::Read, io::SeekFrom, 
 use rust_decimal::prelude::*;
 use clap::Parser;
 use directories::{ProjectDirs};
+use serde::{Deserialize, Serialize};
 use casino::cards::{Card, Value, shoe};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
 
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+struct CasinoState {
+  bankroll: u32
 }
 
 fn main() {
@@ -18,21 +24,21 @@ fn main() {
 
   fs::create_dir_all(data_dir).expect("Couldn't create data dir!");
 
-  let bankroll_path = data_dir.join("bankroll");
+  let state_path = data_dir.join("state");
 
-  let mut bankroll: i32 =
-    match fs::read_to_string(&bankroll_path) {
-      Ok(bankroll_string) => {
-        bankroll_string.trim().parse().unwrap()
+  let mut state: CasinoState =
+    match fs::read_to_string(&state_path) {
+      Ok(state_string) => {
+        toml::from_str(&state_string).unwrap()
       },
       Err(_) => {
-        fs::write(&bankroll_path, "1000\n").expect("Couldn't initialize your bankroll file");
-        1_000
+        fs::write(&state_path, "bankroll = 1000\n").expect("Couldn't initialize your bankroll file");
+        CasinoState{ bankroll: 1_000 }
       }
     };
 
-  println!("Your money: ${bankroll}.00");
-  let mut bet: i32;
+  println!("Your money: ${}.00", state.bankroll);
+  let mut bet: u32;
 
   loop {
     println!("Enter your bet: ");
@@ -42,7 +48,7 @@ fn main() {
     bet = bet_input.trim().parse().unwrap();
     if bet <= 0 {
       println!("Try again, wiseguy")
-    } else if bet > bankroll {
+    } else if bet > state.bankroll {
       println!("You don't have that much money! Try again.");
     } else {
       break;
@@ -96,8 +102,8 @@ fn main() {
         println!("Your hand: {} ({})", hand_to_string(&your_hand), sum);
 
         if sum > 21 {
-          bankroll -= bet.clone();
-          println!("BUST! You lose ${bet}.00. You now have ${bankroll}.00");
+          state.bankroll -= bet.clone();
+          println!("BUST! You lose ${bet}.00. You now have ${}.00", state.bankroll);
           break;
         }
       },
@@ -121,32 +127,32 @@ fn main() {
     }
 
     if dealer_sum > 21 {
-      bankroll += bet.clone();
-      println!("DEALER BUST! You receive ${bet}.00. You now have ${bankroll}.00");
+      state.bankroll += bet.clone();
+      println!("DEALER BUST! You receive ${bet}.00. You now have ${}.00", state.bankroll);
     } else if dealer_sum == player_sum {
       println!("PUSH! Nobody wins.");
     } else if dealer_sum > player_sum {
-      bankroll -= bet.clone();
-      println!("YOU LOSE! You lose ${bet}.00. You now have ${bankroll}.00");
+      state.bankroll -= bet.clone();
+      println!("YOU LOSE! You lose ${bet}.00. You now have ${}.00", state.bankroll);
     } else if your_hand.len() == 2 && player_sum == 21 {
-      let payout = (bet.clone() * 2 / 3);
-      bankroll += payout.clone();
-      println!("BLACKJACK! You receive ${payout}.00. You now have ${bankroll}.00");
+      let payout = bet * 2 / 3;
+      state.bankroll += payout.clone();
+      println!("BLACKJACK! You receive ${payout}.00. You now have ${}.00", state.bankroll);
     } else {
-      bankroll += bet.clone();
-      println!("YOU WIN! You receive ${bet}. You now have ${bankroll}");
+      state.bankroll += bet.clone();
+      println!("YOU WIN! You receive ${bet}. You now have ${}.00", state.bankroll);
     }
 
     if dealer_hand.len() == 2 && dealer_sum == 21 && insurance_flag {
-      let insurance_payout = bet.clone() * 2i32;
-      bankroll += insurance_payout.clone();
-      println!("DEALER BLACKJACK! Your insurance bet pays out ${insurance_payout}.00. You now have ${bankroll}.00.");
+      let insurance_payout = bet * 2;
+      state.bankroll += insurance_payout.clone();
+      println!("DEALER BLACKJACK! Your insurance bet pays out ${insurance_payout}.00. You now have ${}.00.", state.bankroll);
     }
   }
 
 
-  if bankroll.is_zero() {
-    bankroll += 1_000;
+  if state.bankroll.is_zero() {
+    state.bankroll += 1_000;
     println!("* Unfortunately, you've run out of money.");
     println!("* However, a portly gentleman in a sharp suit was watching you play your final hand.");
     println!("* He says \"I like your moxie, kiddo. Take this, and be a little more careful next time. This stuff doesn't grow on trees.\"");
@@ -154,7 +160,7 @@ fn main() {
     println!("* The man hands you $1000.00.");
   }
 
-  fs::write(bankroll_path, bankroll.to_string().as_bytes());
+  fs::write(state_path, toml::to_string(&state).unwrap());
 }
 
 fn blackjack_sum(hand: &Vec<Card>) -> u8 {
